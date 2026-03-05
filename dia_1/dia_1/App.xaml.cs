e075c08bfa0b9619919f8c40b2d417fdb7ec2d28
Jorge Oliveira <kaatancontos@gmail.com>
2026-03-05 04:10:37 -0300

    05032026_1

---
+++ dia_1/dia_1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace dia_1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
